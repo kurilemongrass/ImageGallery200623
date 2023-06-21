2fe8091d0db25352f1bfee12e0454e344be82f1b
kurifung01 <kurifung01@gmail.com>
2023-06-21 07:13:12 -0400

    change image id 1 url

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: 1,
-    url: "../assets/Taikwun.jpeg",
+    url: "https://lh3.googleusercontent.com/HEfcVy-OL7N4V6zw2B-yThivHAHkMhSl-O-kZKhZKX-gLTv_KgtP2g_pbsSctGfd3d4NMozrrxm8o4ZnLdIN6K4BkX_wvBtB3lm-bqgzhDevUaEv9PbkhuU_s6wzovF1laFivKuwJ9j4S7RhPA2Wdz3Uw9GhCqj2ZGIA_C2rFGQN_sWQ8EfSwF5p24D2-RjZXibRgGw0hTprV97lxAQ5EgeRjYh2ab3UW4j25uBqlRcMu79BwhycoUmD0QdH4HrDG9oLtjuboqtR1AkjTttd9bnk8Vg4XNlJSOitN9r-OUfkfl4A_dOcg12q18ke4W0dURdiUcUCSJVeDGEKUkBpevyeCjyi2a1kfTKdXcuIAbUzCWwBhGmCtPo2CK-mGS3sc56pDExUqDgsFFmvL68k7AZlsck_afRKSJlV9MFBne13lbi1eF1hA6hwfRC0bX01SLLRBqIxg369tNf4InxwEaKwF7m0r2r-Gwj4YsVE-yx2vemerSZTz_ud5SR7HRA2MkOHJDilSJJ9aOvPI7-87KqmGLkX7YUbO3axfwU0zP9fgwAuMjEe75VJKJQpPxPsPtJU6hTMxgYMaQMukb2DG91ypi_2s5af7tWNka0-exeCIvI-WQSR7PPLaZs_lZbSfRrAA_0B7rCyq9LfyN-Jy7IVKtXShcGqk_ymingYfrVld6ocTdWUr9EBNfA1xZatuIIE6aUxcVcsAixIgzSRoOhRnRVptmtlAy0X3nO0ZpsF0oNhRfS9E153uOALcZjloV2oC2mBqhCM_COyqo0G6ndLisFZg71mIkfpPoZDtj6uwtbXhwj40yhtuNkSO_Wp9qX8KcRkuVxIlk4tJnuLlZ6Gaw4eyVOM2fD8OaYawgE4jDsGXW6JCrSgWTC7QdD-2LdK-36OQi5dQfzHmz9kog49_YFzJn7X4yj-DSde35PXeLM=w750-h600-s-no?authuser=0",
     title: "Tai Kwun",
     year: 2022,
     photographer: "Kuri F",
