0809ba188acb28d6539e50264992b91e9d87ff71
kurifung01 <kurifung01@gmail.com>
2023-06-21 00:44:14 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ImageGallery200623",
+  base: "/ImageGallery200623/",
   plugins: [react()],
 });
